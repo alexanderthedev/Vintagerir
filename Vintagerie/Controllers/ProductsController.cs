4e07dee012d8c602e47ee7c1fb27e6f26d66b5dc
alexanderthedev <alexprift@yahoo.gr>
2016-12-25 17:29:11 +0100

    d

--- Vintagerie/Controllers/ProductsController.cs
+++ Vintagerie/Controllers/ProductsController.cs
@@ -293,7 +293,7 @@ namespace Vintagerie.Controllers
             var product = _context.Products.Include(i => i.User).Single(s => s.Slug == slug);
             var likeThis = _context.Likes.Select(l => l.LikerId).Contains(userId);
             var loveThisStre = _context.Loves.Select(l => l.LovedId).Contains(userId);
-            var picturesOfProduct = _context.PIctureInfos.Where(p => p.Product.Slug == slug && p.UserId == userId).ToList();
+            var picturesOfProduct = _context.PIctureInfos.Where(p => p.Product.Slug == slug).ToList();
 
             var viewModel = new SingleProductViewModel
             {
